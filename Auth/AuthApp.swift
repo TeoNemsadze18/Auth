704614bf59f3477f87e93071ea8526dad727bdec
TeoNemsadze <teona.nemsadze.2@iliauni.edu.ge>
2023-10-31 15:57:26 +0400

    Initial Commit

---
+++ Auth/AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthApp.swift
+//  Auth
+//
+//  Created by teona nemsadze on 31.10.23.
+//
+
+import SwiftUI
+
+@main
+struct AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
